da32cd99d2c4789156a71418ad2a4f3cec481f2b
Watts <jamie.watts@stfc.ac.uk>
2024-05-14 14:11:21 +0100

    qt now works in python. Started adding funtionality to buttons.

--- run_gui.py
+++
@@ -1,9 +0,0 @@
-import sys
-from FTIR_qt_interface import Ui_MainWindow as GUI
-from PyQt5.QtWidgets import QApplication
-
-app = QApplication(sys.argv)
-UIWindow = GUI()
-UIWindow.show()
-
-app.exec_()
